624969f1a7a85b54f505d43d7a0e7e95d66f4f21
Mardan901 <kl2412018292@student.uptm.edu.my>
2026-06-06 15:03:04 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$host = 'bwim1tulidk8xdcvwgwi-mysql.services.clever-cloud.com'; 
-$user = 'uljgwdxtxae61kuv';
-$dbname = 'bwim1tulidk8xdcvwgwi';
+$host = "bwim1tulidk8xdcvwgwi-mysql.services.clever-cloud.com"; 
+$user = "uljgwdxtxae61kuv";
+$dbname = "bwim1tulidk8xdcvwgwi";
 
-$pass = '3osNtILcnRLKeNIIGnC1'; 
+$pass = "0TAUku0kWyilR8hrl5R8"; 
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
