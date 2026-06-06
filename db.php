c02db612c547f7028a1f498722c8592598ce16cb
Mardan901 <kl2412018292@student.uptm.edu.my>
2026-06-06 14:32:56 +0800

    db.php

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";   
-$password = "";       
-$dbname = "steel_db"; 
+$host = 'bwim1tulidk8xdcvwgwi-mysql.services.clever-cloud.com'; 
+$user = 'uljgwdxtxae61kuv';
+$dbname = 'bwim1tulidk8xdcvwgwi';
 
-// Create the connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Click the little orange padlock icon next to Password on your screen to copy your password!
+$pass = 'IkBlDd8CQ0QTawnrS3Hs'; 
 
-// Check if the connection works
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
