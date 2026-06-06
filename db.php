028e3cf6ae1313d7ea0b1968e8235b24c0476a23
Mardan901 <kl2412018292@student.uptm.edu.my>
2026-06-06 16:33:03 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,22 @@
 <?php
-$host = "bwim1tulidk8xdcvwgwi-mysql.services.clever-cloud.com"; 
-$user = "uljgwdxtxae61kuv";
-$dbname = "bwim1tulidk8xdcvwgwi";
-
-$pass = "0TAUku0kWyilR8hrl5R8"; 
-
-$conn = mysqli_connect($host, $user, $pass, $dbname);
+// Initialize a secure SQL connection object
+$conn = mysqli_init();
 
 if (!$conn) {
+    die("mysqli_init failed");
+}
+
+$host = 'steel-db-steel-db.g.aivencloud.com'; 
+$user = 'avnadmin';
+$pass = 'AVNS_FuuyThp7HaLjxiETeCv';
+$dbname = 'defaultdb';
+$port = 18828;
+
+
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+// Connect using the configured parameters
+if (!mysqli_real_connect($conn, $host, $user, $pass, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
