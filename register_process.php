3049d7813e37a93c2acf1d9bc0068fde4630716d
LUQMAN HAKEEM <lqmsy06@gmail.com>
2026-06-14 23:19:21 +0800

    Update register_process.php

--- register_process.php
+++ register_process.php
@@ -15,6 +15,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<h3 style='color:red;'>Error: That username is already taken!</h3>";
         echo "<a href='register.html'>Click here to try a different one.</a>";
     } else {
+        
+        if ($role === 'admin') {
+            $admin_code = $_POST['admin_verification_code'];
+            if ($admin_code !== 'STEEL_ADMIN_2026') {
+                die("<h3 style='color:red;'>Error: Unauthorized admin registration attempt. Invalid secret code.</h3> <br><br> <a href='register.html'>Go back</a>");
+            }
+        }
+        // --- END SECURITY CHECK ---
+
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
         $sql = "INSERT INTO users (username, password, role) 
